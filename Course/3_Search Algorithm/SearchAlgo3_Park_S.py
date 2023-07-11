c1706809a0e33510f71bc7261afa040ddf4c71fb
chocoIcecream34 <137990965+chocoIcecream34@users.noreply.github.com>
2023-07-11 23:59:46 +0900

    수정예정

---
+++ Course/3_Search Algorithm/SearchAlgo3_Park_S.py
@@ -0,0 +1,17 @@
+#백준 1654
+import sys
+
+def binary_search(cable_len: list, target_count: int):
+    # 케이블 최대 길이 구하기
+    start = 1
+    end = max(cable_len)
+    while start <= end:
+        mid = (start + end) // 2
+        cable_count = [(i // mid) for i in cable_len]
+        # 길이를 더 짧게
+        if sum(cable_count) < target_count:
+            end = mid - 1
+        # 길이를 더 길게
+        elif sum(cable_count) >= target_count:
+            start = mid + 1
+    print(end)
