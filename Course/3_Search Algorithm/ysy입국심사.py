aaa7bd395d2310b3e98e6f237bf8a59cfc1ef22d
iioese <114419552+iioese@users.noreply.github.com>
2023-07-24 11:59:27 +0900

    Update ysy입국심사.py

--- Course/3_Search Algorithm/ysy입국심사.py
+++ Course/3_Search Algorithm/ysy입국심사.py
@@ -4,19 +4,19 @@ def solution(n, times): # 사람 수 n, 심사관 당 소요 시간 배열 times
     l = 1
     r = max(times) * n # 최대 소요 시간
     
-    while l <= r :
+    while l <= r:
         m = (l + r) // 2 # 가운데 m
         ok = 0 # 심사완료 사람 수 ok
      	
-        for time in times :
+        for time in times:
             ok += m // time
-            if ok >= n :
+            if ok >= n:
                 break
         
-        if ok >= n : # 크거나 같을 때
+        if ok >= n: # 크거나 같을 때
             answer = m
             r = m - 1
-        else : # 작을 때
+        else: # 작을 때
             l = m + 1
     
     return answer
