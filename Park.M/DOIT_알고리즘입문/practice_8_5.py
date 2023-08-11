4255d356fb7eea7a42fd0f2d84134a2de3299997
m2nseop <mini071663@gmail.com>
2023-08-11 17:50:05 +0900

    .

--- Park.M/DOIT_알고리즘입문/practice_8_5.py
+++ Park.M/DOIT_알고리즘입문/practice_8_5.py
@@ -134,3 +134,48 @@ class DoubleLinkedList:
         while not self.is_empty():                          #리스트 전체가 빌 때까지
             self.remove_first()                             #머리 노드를 삭제
         self.no = 0
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        """이터레이터를 반환"""
+        return DoubleLinkedListIterator(self.head)
+    
+    def __reversed__(self) -> DoubleLinkedListReverseIterator:
+        """내림차순 이터레이터를 반환"""
+        return DoubleLinkedListReverseIterator(self.head)
+    
+class DoubleLinkedListIterator:
+    """DoubleLinkedList의 이터레이터용 클래스"""
+
+    def __init__(self, head: Node):
+        self.head = head
+        self.current = head.next
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        return self
+    
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
+        
+class DoubleLinkedListReverseIterator:
+    """DoubleLinkedList의 내림차순 이터레이터 클래스"""
+
+    def __init__(self, head: Node):
+        self.head = head
+        self.current = head.prev
+
+    def __iter__(self) -> DoubleLinkedListReverseIterator:
+        return self
+    
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.prev
+            return data
+        
